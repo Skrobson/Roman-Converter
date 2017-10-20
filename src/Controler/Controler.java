bc1b35c27fe1d1b03b4a8f374c140705e078b923
Bartłomiej Skrobol <skrobolbartlomiej@gmail.com>
2017-10-20 17:55:43 +0200

    create MVC packages

--- src/Controler/Controler.java
+++ src/Controler/Controler.java
@@ -3,7 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package romanconverter;
+package Controler;
+
+import View.*;
+import Model.*;
+
 
 /**
 * @author Skrobol Bartłomiej
