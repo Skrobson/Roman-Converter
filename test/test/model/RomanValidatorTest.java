a761a526203731e7d100772625c2a7af878148bc
Bartłomiej Skrobol <skrobolbartlomiej@gmail.com>
2017-10-27 13:10:37 +0200

    refactor tests for treshold

--- test/test/model/RomanValidatorTest.java
+++ test/test/model/RomanValidatorTest.java
@@ -3,6 +3,7 @@ package test.model;
 
 
 import org.junit.*;
+import java.util.*;
 
 import Model.RomanNumberValidator;
 import org.junit.Assert;
@@ -16,22 +17,67 @@ import org.junit.Assert;
  */
 public class RomanValidatorTest {
   
-    RomanNumberValidator validator = new RomanNumberValidator();
+    private RomanNumberValidator validator = new RomanNumberValidator();
+    static private List<String> validNumbersList;
+    static private List<String> invalidNumbersList;
+
+    @BeforeClass
+    static public void prepareValidNumbersList(){
+        validNumbersList = new LinkedList<>();
+        validNumbersList.add("I" );
+        validNumbersList.add("IV" );
+        validNumbersList.add("III" );
+        validNumbersList.add("VI" );
+        validNumbersList.add("X" );
+        validNumbersList.add("L" );
+        validNumbersList.add("XL" );
+        validNumbersList.add("C" );
+        validNumbersList.add("CX" );
+        validNumbersList.add("D");
+        validNumbersList.add("m");
+        validNumbersList.add("mmm");
+        validNumbersList.add("CMXCIX");
+        
+    }
     
-   
+    @BeforeClass
+    static public void prepareInvalidNumbersList(){
+        invalidNumbersList = new LinkedList<>();
+        invalidNumbersList.add("IIII" );
+        invalidNumbersList.add("IIV" );
+        invalidNumbersList.add("VIIII" );
+        invalidNumbersList.add("VX" );
+        invalidNumbersList.add("XVV" );
+        invalidNumbersList.add("VL" );
+        invalidNumbersList.add("XIL" );
+        invalidNumbersList.add("CCCC" );
+        invalidNumbersList.add("LL" );
+        invalidNumbersList.add("MDM");
+        invalidNumbersList.add("mA");
+        invalidNumbersList.add("mmmm");
+        invalidNumbersList.add("AAAAAAA");
+        
+    }
+    
     @Test
     public void invalidNumberFormat(){
         
         
-        String number = "kk";
-        Assert.assertFalse(number + " is wrong roman nr", validator.checkNumber(number));
+        
+        for(String number : invalidNumbersList){
+             Assert.assertFalse(number , validator.checkNumber(number));
+        }
     }
+    
+    
     
     @Test
     public void validNumberFormat(){
-    
-        String number = "III";
-        Assert.assertTrue(validator.checkNumber(number));
+                    
+        for(String number : validNumbersList){
+            Assert.assertTrue(number, validator.checkNumber(number));
+            
+        }
     }
  
 }
