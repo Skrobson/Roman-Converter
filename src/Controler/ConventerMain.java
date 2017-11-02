891f1b6cbc3e85fe497f8422c4e8477ff998d139
Bartłomiej Skrobol <skrobolbartlomiej@gmail.com>
2017-11-02 09:34:06 +0100

    checks number of arguments in main function some changes in Javadoc

--- src/Controler/ConventerMain.java
+++ src/Controler/ConventerMain.java
@@ -1,35 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controler;
 
 
 /**
  *
-* @author Skrobol Bartłomiej
- * @version 1.0
+ * @author Skrobol Bartłomiej
+ * @version 2.0
+ * Main class, checks program arguments, run controler  
  */
 public class ConventerMain {
 
     /**
+     * Main function of application
+     * Cheks program arguments and run controler
      * @param args the command line argument should be roman number
-     *
      */
     public static void main(String[] args) {
         
         Controler controler = new Controler();
-    
-        
-       try{           
+            
+       if(args.length == 1){      
             controler.run(args[0]);
         }
-        catch(ArrayIndexOutOfBoundsException e){
+       else{
             controler.run();
-        }
-        
-                  
+        }              
     }
     
 }
